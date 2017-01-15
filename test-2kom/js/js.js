43fe5dac022716228af4bdf4c318d48dbec7c4ee
enovikova <novikova9835596@gmail.com>
2017-01-15 10:45:38 +0300

    spaceford test

---
+++ test-2kom/js/js.js
@@ -0,0 +1,58 @@
+var main = function () {
+    // курсор
+    $('.evaluation_positive').awesomeCursor('thumbs-o-down');
+    $('.evaluation_negatie').awesomeCursor('thumbs-o-up');
+
+    //   сворачивание 
+    $('.tipe1').click(function () {
+        $('.container').hide();
+        $('.userinfo').hide();
+    });
+
+    $('.tipe2').click(function () {
+        $('.container').animate({
+            width: '1'
+        }, 600);
+        $('.usercontent_menu').hide();
+        $('.usercontent').hide();
+        $('.buttons_wrap').addClass('small');
+    });
+    $('.tipe3').click(function () {
+        $('.container').animate({
+            width: '50%'
+        }, 600);
+        $('.usercontent_menu').show();
+        $('.usercontent').show();
+        $('.buttons_wrap').removeClass('small');
+    });
+
+    // carousel
+    $('.owl-carousel').owlCarousel({
+        
+        loop:true,
+        dots:false,
+        margin: 12,
+        items: 3,
+        navText: '<>',
+        responsive: {
+            0: {
+                nav:false
+            },
+            1200: {
+               nav:true
+            }
+        }
+    });
+
+    // tabs    
+    $(".usercontent_menu li a").click(function(event) {
+        event.preventDefault();        
+        $(this).parents(".usercontent_menu").find('a').removeClass("active");
+        $(this).addClass("active");
+        var tab = $(this).attr("href");
+        $(".usercontent_tab").not(tab).removeClass("active");
+        $(tab).addClass("active");
+    });    
+}
+
+$(document).ready(main);
---
+++ test-2kom/js/sample.js
@@ -0,0 +1,39 @@
+var main = function() {
+
+  $(window).scroll(function(){
+      if ($(window).width() > 1026) {
+         if ($(this).scrollTop() > 560) {
+            $('.heading').addClass('relative');
+            $('.subheading').addClass('relative');
+            $('.block_two').addClass('scroll');
+            
+        } else {
+                $('.heading').removeClass('relative');
+                $('.subheading').removeClass('relative');
+                $('.block_two').removeClass('scroll');
+                
+        };
+        if ($(this).scrollTop() > 750) {
+            $('.header').addClass('small_header');
+        } else {
+            $('.header').removeClass('small_header');
+        }
+      } else {
+          $('.heading').addClass('mob');
+            $('.subheading').addClass('mob');
+            $('.block_two').addClass('mob');
+      }
+        
+  });
+}
+
+$(document).ready(main);
+
+  $('.tipe2').click(function(){
+        $('.container').addClass('small');
+    });
+    $('.tipe3').click(function(){
+        $('.container').removeClass('small');
+    });
+
+            
